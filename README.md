66192f06f4f72c60d3cc513431830fbe06fcffd1
Nicholay Nascimento <ningyo-tsukai@users.noreply.github.com>
2014-02-26 22:04:21 -0300

    Achieved basic functionality, editied README

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Clink
 =====
 
-Clink is a CLI tool written in Python for storing and organizing URLs in simple text files. Its name stands for "collect link".
+Clink is a CLI tool written in Python for simple URL-keeping using plaintext files. The name stands for 'collect link'. At the moment, Clink simply dumps the contents of the clipboard and a supplied descriptive title to a 'links.txt' file in the current directory. So in other words, it's not ready for practical use yet. I intend to implement tagging, viewing/sorting and searching functionality eventually. Since Clink uses the _Pyperclip_ module by Al Sweigart for clipboard handling, it has the potential to be cross-platform.
+
+The basic usage (while URL is in clipboard) is:
+
+`clink.py <title>`
--- clink.py
+++ clink.py
@@ -1,18 +1,26 @@
+#! /usr/bin/env python
+
 # Clink - the CLI URL collector
 # Author: Nicholay Nascimento
 
-import pyperclip
+import pyperclip, os, sys
 
-file = open('links.txt', 'a')
+nargs = len(sys.argv)    # Will be useful later when I figure out argument parsing
+linksfile = open('links.txt', 'a')
+
+def printUsage():
+    print("Usage is: %s <title>" % os.path.basename(sys.argv[0]))
 
 def addLink():
-    title = "Test"
-    url = pyperclip.paste()
-
-    print "URL is: "+url
-    if (url):
-        file.write(title+"\n"+url+"\n")
+    if (nargs>1):
+        title = sys.argv[1]
+        url = pyperclip.paste()    
+        if (url):
+            linksfile.write(title+"\n"+url+("\n"*2))
+        else:
+            print("The clipboard is empty.")
     else:
-        print "The clipboard is empty."
-
+         printUsage()
+         
 addLink()
+linksfile.close()
