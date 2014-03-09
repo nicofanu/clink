ba7830ca636f4a5737b65750644a40a826673468
Nicholay Nascimento <ningyo-tsukai@users.noreply.github.com>
2014-03-09 18:23:01 -0300

    Created linksParser function

--- clink.py
+++ clink.py
@@ -10,22 +10,29 @@ parser = argparse.ArgumentParser(
     epilog="And that's all folks!")
 
 exgroup = parser.add_mutually_exclusive_group()
-exgroup.add_argument("-a", "--add", help="Add a bookmark", metavar="item")
-exgroup.add_argument("-l", "--list", action="store_true", help="List all the bookmarks")
+exgroup.add_argument("-a", "--add", help="add a bookmark", metavar="item")
+exgroup.add_argument("-l", "--list", action="store_true", help="list all the bookmarks")
+#exgroup.add_argument("-v", "--version", action="version", version="%(prog)s version 1.0")
 args = parser.parse_args()
 
+linksfilename = "links.txt"
+
 def addLink(title, url):
-        linksfile = open('links.txt', 'a')
-        linksfile.write(title+"\n"+url+("\n"*2))
-        linksfile.close()
-        print("Added bookmark: "+url)
+    linksfile = open(linksfilename, "a")
+    linksfile.write(title+"\n"+url+("\n"*2))
+    linksfile.close()
+    print("Added bookmark: "+url)
 
 def listLink():
-    linksfile = open('links.txt')
-    filecontents = linksfile.read()
-    print filecontents
+    all_the_links = linksParser()
+    print all_the_links
+
+def linksParser():
+    linksfile = open(linksfilename)
+    filecontents = linksfile.read().strip('\n')
     linksfile.close()
-         
+    return filecontents
+
 if args.add:
     turl = pyperclip.paste()
     if turl:
